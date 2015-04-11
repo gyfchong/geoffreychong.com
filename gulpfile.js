2daae5fbe5c575815d5717383b1e0c37f4b5b76a
Geoffrey Chong <gyf.chong@gmail.com>
2015-04-12 00:28:58 +1000

    restructured file structure + added gulp sass/watch/sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,19 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	watch = require('gulp-watch'),
+	sass = require('gulp-sass'),
+	sourcemaps = require('gulp-sourcemaps');
+ 
+gulp.task('sass', function () {
+	gulp.src('./source/scss/*.scss')
+	.pipe(sourcemaps.init())
+		.pipe(sass())
+	.pipe(sourcemaps.write())
+	.pipe(gulp.dest('./static/css'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('./source/scss/*.scss', ['sass']);
+});
 
 gulp.task('default', function() {
   // place code for your default task here
