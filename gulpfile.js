123d1dffba272a00f9d193da764ed9b66567ec67
Geoffrey Chong <gyf.chong@gmail.com>
2015-04-18 21:11:43 +1000

    Refined gulp processes + added React JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,95 +1,164 @@
-var gulp = require("gulp")
-	ejs = require("gulp-ejs")
-	compass = require("gulp-compass")
-	sourcemaps = require("gulp-sourcemaps")
-	gutil = require("gulp-util")
-	jshint = require("gulp-jshint")
-	uglify = require("gulp-uglify")
-	rename = require("gulp-rename")
-	concat = require("gulp-concat")
-	browserSync = require("browser-sync")
+var gulp = require('gulp')
+	sass = require('gulp-sass')
+	sourcemaps = require('gulp-sourcemaps')
+	gutil = require('gulp-util')
+	jshint = require('gulp-jshint')
+	uglify = require('gulp-uglify')
+	rename = require('gulp-rename')
+	concat = require('gulp-concat')
+	browserSync = require('browser-sync')
 	reload = browserSync.reload;
-	plumber = require("gulp-plumber")
-	notify = require("gulp-notify"),
-	imageop = require('gulp-image-optimization');
+	plumber = require('gulp-plumber')
+	notify = require('gulp-notify'),
+	imageop = require('gulp-image-optimization'),
+	cssGlobbing = require('gulp-css-globbing'),
+	jsxTransform = require('gulp-react'),
+	changed = require('gulp-changed'),
+	clean = require('gulp-rimraf');
 
 
 // Compile SCSS
-gulp.task("scss", function() {
-	return gulp.src("source/assets/css/**/*.scss")
+// Glob all the required SCSS files into 1 styles.css
+gulp.task('scss', function() {
+	return gulp.src('source/assets/css/styles.scss')
 		.pipe(plumber({
 			errorHandler: notify.onError("Error: <%= error.message %>")
 		}))
-		.pipe(compass({
-			css: "build/assets/css",
-			sass: "source/assets/css",
-			sourcemap: true
+		.pipe(cssGlobbing({
+			extensions: ['.scss'],
+			scssImportPath: {
+				leading_underscore: false,
+				filename_extension: false
+			}
 		}))
-		.pipe(gulp.dest("build/assets/css"))
-		.pipe(notify("SCSS: DONE"));
+		.pipe(sourcemaps.init())
+			.pipe(sass())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('build/assets/css'))
+		.pipe(notify('SCSS: DONE'));
 });
 
 // Compile JS:Head
-gulp.task("js:head", function() {
-	return gulp.src([ "source/assets/js/head.js", "source/assets/js/head/*.js"])
+gulp.task('js:head', function() {
+	return gulp.src([ 'source/assets/js/head.js', 'source/assets/js/head/*.js'])
+		.pipe(plumber({
+			errorHandler: notify.onError("Error: <%= error.message %>")
+		}))
+		.pipe(changed('build/assets/js'))
 		.pipe(sourcemaps.init())
-		.pipe(concat("head.js"))
-		.pipe(rename({suffix: ".min"}))
+		.pipe(concat('head.js'))
+		.pipe(rename({suffix: '.min'}))
 		.pipe(uglify())
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest("build/assets/js"))
-		.pipe(notify("JS:HEAD: DONE"));
+		.pipe(gulp.dest('build/assets/js'))
+		.pipe(notify('JS:HEAD: DONE'));
+});
+
+
+// Compile JS:vendor
+gulp.task('js:vendor', function() {
+	return gulp.src('source/assets/js/vendor/*.js')
+		.pipe(plumber({
+			errorHandler: notify.onError("Error: <%= error.message %>")
+		}))
+		.pipe(changed('build/assets/js'))
+		.pipe(gulp.dest('build/assets/js/vendor/'))
+		.pipe(notify('JS:VENDOR: DONE'));
 });
 
 // Compile JS:plugins
-gulp.task("js:plugins", function() {
-	return gulp.src("source/assets/js/plugins/*.js")
+gulp.task('js:plugins', function() {
+	return gulp.src('source/assets/js/plugins/*.js')
+		.pipe(plumber({
+			errorHandler: notify.onError("Error: <%= error.message %>")
+		}))
+		.pipe(changed('build/assets/js'))
 		.pipe(sourcemaps.init())
-		.pipe(concat("plugins.js"))
-		.pipe(rename({suffix: ".min"}))
+		.pipe(concat('plugins.js'))
+		.pipe(rename({suffix: '.min'}))
 		.pipe(uglify())
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest("build/assets/js"))
-		.pipe(notify("JS:PLUGINS: DONE"));
+		.pipe(gulp.dest('build/assets/js'))
+		.pipe(notify('JS:PLUGINS: DONE'));
 });
 
 // Javascript Process
-gulp.task("js:script", function() {
-	return gulp.src([ "source/assets/js/modules/*.js", "source/assets/js/script.js"])
+gulp.task('js:script', function() {
+	return gulp.src(['source/assets/js/**/*.js', 'source/assets/js/script.js', '!source/assets/js/head/*.js', '!source/assets/js/vendor/*.js', '!source/assets/js/plugins/*.js'])
+		.pipe(plumber({
+			errorHandler: notify.onError("Error: <%= error.message %>")
+		}))
+		.pipe(changed('build/assets/js'))
 		.pipe(sourcemaps.init())
-		.pipe(concat("script.js"))
-		.pipe(rename({suffix: ".min"}))
+		.pipe(concat('script.js'))
+		.pipe(rename({suffix: '.min'}))
 		.pipe(uglify())
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest("build/assets/js"))
-		.pipe(notify("JS:SCRIPT: DONE"));
+		.pipe(gulp.dest('build/assets/js'))
+		.pipe(notify('JS:SCRIPT: DONE'));
 });
 
-
+// Optimise images
 gulp.task('images', function(cb) {
-    gulp.src(['source/assets/img/*.png','source/assets/img/*.jpg','source/assets/img/*.gif','source/assets/img/*.jpeg']).pipe(imageop({
-        optimizationLevel: 5,
-        progressive: true,
-        interlaced: true
-    })).pipe(gulp.dest('build/assets/img')).on('end', cb).on('error', cb)
-    .pipe(notify("IMAGE OPTIMISATION: DONE"));
+	gulp.src('source/assets/img/*')
+		.pipe(plumber({
+			errorHandler: notify.onError("Error: <%= error.message %>")
+		}))
+		.pipe(changed('build/assets/img'))
+		.pipe(imageop({
+			optimizationLevel: 5,
+			progressive: true,
+			interlaced: true
+		}))
+		.pipe(gulp.dest('build/assets/img'))
+		.on('end', cb)
+		.on('error', cb)
+		.pipe(notify('IMAGE OPTIMISATION: DONE'));
+});
+
+gulp.task('jsx', function() {
+	return gulp.src('source/**/*.jsx')
+		.pipe(plumber({
+			errorHandler: notify.onError("Error: <%= error.message %>")
+		}))
+		.pipe(sourcemaps.init())
+		.pipe(concat('app.js'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(jsxTransform())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('build/assets/js'))
+		.pipe(notify('JSX TRANSFORM: DONE'));
+});
+
+gulp.task('html:copy', function() {
+	gulp.src('source/*.html')
+		.pipe(changed('build/'))
+    	.pipe(gulp.dest('build/'));
 });
 
 // Start Browsersync server and watch file changes
-gulp.task("server", ["scss","js:head", "js:script", "js:plugins", "images"], function() {
+gulp.task('server', ['html:copy', 'jsx', 'scss', 'js:head', 'js:vendor', 'js:script', 'js:plugins', 'images'], function() {
 	browserSync({
-		server: "build"
+		server: 'build'
 	});
 
-	gulp.watch("source/assets/css/**/*.scss", ["scss"]);
-	gulp.watch("source/assets/js/**/*.js", ["js:head", "js:script", "js:plugins"]);
-	gulp.watch("source/assets/img/*", ["images"]);
-	// gulp.watch("source/**/*.ejs", ["ejs"]);
+	gulp.watch('source/*.html', ['html:copy']);
 
-	gulp.watch("build/**/*.css").on("change", reload);
-	gulp.watch("build/**/*.js").on("change", reload);
-	gulp.watch("build/**/*.html").on("change", reload);
-	gulp.watch("build/**/img/*").on("change", reload);
+	gulp.watch('source/assets/css/styles.scss', ['scss']);
+	
+	gulp.watch('source/assets/js/head.js', ['js:head']);
+	gulp.watch('source/assets/js/*.js', ['js:vendor']);
+	gulp.watch('source/assets/js/plugins.js', ['js:plugins']);
+	gulp.watch('source/assets/js/script.js', ['js:script']);
+
+	gulp.watch('source/assets/img/*', ['images']);
+
+	gulp.watch('source/**/*.jsx', ['jsx']);
+
+	gulp.watch('build/assets/css/*.css').on('change', reload);
+	gulp.watch('build/assets/js/*.js').on('change', reload);
+	gulp.watch('build/*.html').on('change', reload);
+	gulp.watch('build/assets/img/*').on('change', reload);
 });
 
 
