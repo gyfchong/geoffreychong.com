d643a10d4e4dbd0a5fbb23e2f7edc089edc49e52
gyfchong <me@geoffreychong.com>
2020-09-06 01:08:06 +1000

    added: responsive

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   "modules": false,
   "map": process.env.NODE_ENV === 'production' ? false : true,
   plugins: [
+    require('postcss-nested'),
     require('autoprefixer'),
   ],
 };
