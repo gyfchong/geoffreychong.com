4315fe698e859d32bbb4d67548e923a26c048378
Geoffrey Chong <gyf.chong@gmail.com>
2023-04-09 21:55:17 +1000

    fix: postcss

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  "modules": false,
-  "map": process.env.NODE_ENV === 'production' ? false : true,
-  plugins: [
-    require('postcss-nested'),
-    require('autoprefixer'),
-  ],
-};
